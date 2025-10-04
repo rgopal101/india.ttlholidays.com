ca4c292b1b1fb9ce9032e66d50ce8e5c74cc8501
Ram Gopal <rgopal101@gmail.com>
2025-10-05 02:05:20 +0530

    Update from home

--- Pages/VisaDetail.cshtml.cs
+++ Pages/VisaDetail.cshtml.cs
@@ -6,6 +6,7 @@ namespace india.ttlholidays.com.Pages
     public class VisaDetailModel : PageModel
     {
         public string Country { get; set; } = string.Empty;
+        public string bgimage = "assets/img/innerpages/breadcrumb-bg7.jpg"; 
         public void OnGet(string? country)
         {
             Country = country ?? "India";
