32e7a6d8881bf78ca8d27003cb275dda8f6374bf
Ram Gopal <rgopal101@gmail.com>
2025-10-14 15:17:29 +0530

    flight and flightdetail page change

--- Pages/Flight.cshtml.cs
+++ Pages/Flight.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace india.ttlholidays.com.Pages
 {
     public class FlightModel : PageModel
     {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            public string Depart { get; set; }
+             
+        }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            // Handle form submission logic here
+            return RedirectToPage();
         }
     }
 }
